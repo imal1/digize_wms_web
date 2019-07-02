6bfb4ad16244b41dfcc6f0011c52c1e633110049
imali <chjczj@126.com>
2019-07-02 22:18:31 +0800

    router + sass + router + loginPage initial comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './pages/login';
+import './index.scss';
+import App from './pages/router';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,78 @@
-import React from 'react';
-import { Button } from 'antd';
-import './index.css';
+import React from "react";
+import { Button, Row, Col, Card, Form, Input, Icon, Typography, Divider } from "antd";
+import "./index.scss";
 
-class LoginPage extends React.Component {
+const { Title } = Typography;
+
+class LoginPage extends React.Component<any, any> {
+  handleSubmit = () => {
+
+  }
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div className="LoginPage">
-        <Button type="primary">Button</Button>
+      <div className="loginPage">
+        <Row>
+          <Col span={18}>
+            <div>物流WMS平台</div>
+          </Col>
+          <Col span={6} className="login-wrapper">
+            <Card className="login-card">
+              <Title level={4}>鼎泽后台管理</Title>
+              <Divider />
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Item>
+                  {getFieldDecorator("username", {
+                    rules: [
+                      { required: true, message: "Please input your username!" }
+                    ]
+                  })(
+                    <Input
+                      prefix={
+                        <Icon
+                          type="user"
+                          style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                      }
+                      placeholder="用户编码"
+                    />
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator("password", {
+                    rules: [
+                      { required: true, message: "Please input your Password!" }
+                    ]
+                  })(
+                    <Input
+                      prefix={
+                        <Icon
+                          type="lock"
+                          style={{ color: "rgba(0,0,0,.25)" }}
+                        />
+                      }
+                      type="password"
+                      placeholder="密码"
+                    />
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    block={true}
+                    htmlType="submit"
+                    className="login-form-button"
+                  >
+                    立即登入
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
 }
 
-export default LoginPage;
+export default Form.create()(LoginPage);
